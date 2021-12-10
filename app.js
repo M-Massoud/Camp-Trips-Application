bf4ab161b2ee2adcb95b89b83ee633cbe16e724c
M-Massoud <you@example.com>
2021-12-10 22:03:35 +0200

    ready to deploy after solving errors hopefulllly

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const upload = multer({
 });
 
 
-const mbxGeocoding = require('@mapbox/mapbox-gl-geocoder');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({
     accessToken: mapBoxToken
